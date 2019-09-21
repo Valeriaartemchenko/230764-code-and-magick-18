a4e519a4ac227db5aeb5c41af383198166d308a4
Valeria Artemchemko <valeria.artemchenko@gmail.com>
2019-09-21 12:23:23 +0200

    Вывод гистограммы

--- js/stat.js
+++ js/stat.js
@@ -1,66 +1,81 @@
-var cloudX = 100;
-var cloudY = 10;
-var cloudWidth = 420;
-var cloudHeight = 270;
-var cloudColor = '#ffffff';
-var textColor ='#000000';
-var shadeColor = 'rgba(0, 0, 0, 0.7)';
-var shadeX = cloudX + 10;
-var shadeY = cloudY + 10;
-//const MESSAGE = ’Ура вы победили!\nСписок результатов:’;
+'use strict';
 
+var CLOUD_POSITION = {
+  x: 100,
+  y: 10
+};
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_COLOR = '#ffffff';
+var CLOUD_SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
+var CLOUD_SHADOW_OFFSET = 10;
+
+var CLOUD_TEXT_FAMILY = 'PT Mono';
+var CLOUD_TEXT_COLOR = '#000000';
+var CLOUD_TEXT_SIZE = 16;
+var CLOUD_TEXT_PADDING = 15;
+var CLOUD_TEXT = ['Ура вы победили!', 'Список результатов:'];
+
+var HISTOGRAM_COLUMN_WIDTH = 40;
+var HISTOGRAM_COLUMN_HEIGHT = 150;
+var HISTOGRAM_COLUMN_MARGIN = 50;
+var HISTOGRAM_MARGIN_LEFT = 70;
+var RANDOM_SATURATION = Math.floor(Math.random() * 100 + 1);
+var HISTOGRAM_COLUMN_COLOR_SELF = 'rgba(255, 0, 0, 1)';
+var HISTOGRAM_TEXT_INTERVAL = 5;
+
+var SELF_NAME = 'Вы';
+var FIELD_HEIGHT = 250;
+
+var getPlayerColour = function (name) {
+  return name === SELF_NAME ? HISTOGRAM_COLUMN_COLOR_SELF : 'hsl(240, ' + RANDOM_SATURATION + '%, 50%)';
+};
+
+var getMaxTime = function (times) {
+  var maxTime = -1;
+
+  for (var i = 0; i < times.length; i++) {
+    if (maxTime < times[i]) {
+      maxTime = times[i];
+    }
+  }
+  return maxTime;
+};
 
 window.renderStatistics = function (ctx, names, times) {
-
   drawCloud(ctx);
   drawText(ctx);
-
+  drawHistogram(ctx, names, times);
 };
 
 var drawCloud = function (ctx) {
-  ctx.fillStyle = shadeColor;
-  ctx.fillRect(shadeX, shadeY, cloudWidth, cloudHeight);
-  ctx.fillStyle = cloudColor;
-  ctx.fillRect(cloudX, cloudY, cloudWidth, cloudHeight);
+  ctx.fillStyle = CLOUD_SHADOW_COLOR;
+  ctx.fillRect(CLOUD_POSITION.x + CLOUD_SHADOW_OFFSET, CLOUD_POSITION.y + CLOUD_SHADOW_OFFSET, CLOUD_WIDTH, CLOUD_HEIGHT);
+
+  ctx.fillStyle = CLOUD_COLOR;
+  ctx.fillRect(CLOUD_POSITION.x, CLOUD_POSITION.y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 var drawText = function (ctx) {
-  ctx.fillStyle = textColor;
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 150, 30);
-  ctx.fillText('Список результатов:', 150, 50)
+  ctx.font = CLOUD_TEXT_SIZE + 'px ' + CLOUD_TEXT_FAMILY;
+  ctx.fillStyle = CLOUD_TEXT_COLOR;
+
+  for (var i = 0; i < CLOUD_TEXT.length; i++) {
+    ctx.fillText(CLOUD_TEXT[i], CLOUD_POSITION.x + CLOUD_TEXT_PADDING, CLOUD_POSITION.y + (CLOUD_TEXT_SIZE + CLOUD_TEXT_PADDING) * (i + 1));
+  }
 };
 
-
-/*
-
-
-
-ctx — канвас на котором рисуется игра.
-
-names — массив, с именами игроков прошедших уровень. Имя самого игрока — Вы. Массив имён формируется случайным образом.
-
-times — массив, по длине совпадающий с массивом names. Массив содержит время прохождения уровня соответствующего игрока из массива names. Время прохождения уровня задано в миллисекундах.
-
-Эта функция будет вызываться каждый раз когда игрок проходит уровень. Чтобы успешно пройти уровень, надо выстрелить фаерболом (клавиша Shift) в забор.
-
-При вызове этой функции на канвас ctx должны быть выведены следующие элементы:
-
-
-После сообщения о победе должна располагаться гистограмма времён участников. Параметры гистограммы следующие:
-
-Высота гистограммы 150px.
-
-Ширина колонки 40px.
-
-Расстояние между колонками 50px.
-
-Цвет колонки игрока Вы rgba(255, 0, 0, 1).
-
-Цвет колонок других игроков — синий, а насыщенность задаётся случайным образом.
-
-Обратите внимание. В rgba последний параметр — это прозрачность, а не насыщенность. Поэтому для задания цвета колонок других игроков нужно использовать hsl.
-Обратите внимание. Функцию отрисовки статистики вызывать не надо. Её будет вызывать непосредственно сама игра из файла js/game.js.
-Обратите внимание. Время прохождения игры должно быть округлено к целому числу.
-
- */
+var drawHistogram = function (ctx, names, times) {
+  var maxTime = getMaxTime(times);
+  for (var i = 0; i < times.length; i++) {
+    var histogramColumnX = HISTOGRAM_MARGIN_LEFT + (HISTOGRAM_COLUMN_MARGIN + HISTOGRAM_COLUMN_WIDTH) * (i + 1);
+    var histogramColumnY = FIELD_HEIGHT - HISTOGRAM_COLUMN_HEIGHT;
+    var height = HISTOGRAM_COLUMN_HEIGHT * (times[i] / maxTime);
+    ctx.fillStyle = getPlayerColour(names[i]);
+    ctx.fillRect(histogramColumnX, FIELD_HEIGHT - height, HISTOGRAM_COLUMN_WIDTH, height);
+    ctx.fillStyle = CLOUD_TEXT_COLOR;
+    ctx.fillText(Math.floor(times[i]), histogramColumnX, histogramColumnY - HISTOGRAM_TEXT_INTERVAL);
+    ctx.fillText(names[i], histogramColumnX, FIELD_HEIGHT + HISTOGRAM_TEXT_INTERVAL + CLOUD_TEXT_SIZE);
+  }
+};
